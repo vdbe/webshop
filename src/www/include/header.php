1a391cb7e58e2fb28780c932481e07f539967c30
vdbe <vdbewout@gmail.com>
2021-12-26 12:20:53 +0100

    favicon

--- src/www/include/header.php
+++ src/www/include/header.php
@@ -13,14 +13,16 @@ require_once __DIR__ . '/php_header.php';
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <title>&#35;&#36;&#64;&#33;&#37; CSS</title>
 </head>
 
 <body>
     </nav>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="/#">Site Name</a>
+            <a class="navbar-brand" href="/#">&#35;&#36;&#64;&#33;&#37; CSS</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
